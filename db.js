3f52ecbf710ad700b6f69e69f9d741bd3822b3cd
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2024-02-16 10:09:45 +0000

    chore: add comments

--- db.js
+++ db.js
@@ -2,10 +2,15 @@ import { readFileSync, writeFileSync } from 'fs';
 const filename = 'db.json';
 
 export function saveDB (dbName, data) {
+  // reads the file
   const db = JSON.parse(readFileSync(filename, 'utf-8') || '{}');  
+  // if db is not there, create an empty object
   if (!db) db = {}
+  // if a particular database is not there, create an empty object
   if (!db[`${dbName}`]) db[`${dbName}`] = {}
+  // set the data with the id as the key, basically rewriting whatever is in there
   db[`${dbName}`][`${data.id}`] = data;
+  // overwrites the file with the new data
   writeFileSync(filename, JSON.stringify(db, null, 2), 'utf-8');
 }
 
@@ -13,7 +18,9 @@ export function readDB (dbName, id = null) {
   const db = JSON.parse(readFileSync(filename, 'utf-8') || '{}');
   if (!db) db = {}
   if (!db[`${dbName}`]) db[`${dbName}`] = {}
+  // if there is id, just return that one
   if (id) return db[`${dbName}`][`${id}`]
+  // if there is no id, return as an array of values
   return Object.values(db[`${dbName}`])
 }
 
@@ -21,6 +28,7 @@ export function deleteItemDB (dbName, id) {
   const db = JSON.parse(readFileSync(filename, 'utf-8') || '{}');  
   if (!db) db = {}
   if (!db[`${dbName}`]) db[`${dbName}`] = {}
+  // deletes that id from the database
   delete db[`${dbName}`][`${id}`]
   writeFileSync(filename, JSON.stringify(db, null, 2), 'utf-8');
 }
