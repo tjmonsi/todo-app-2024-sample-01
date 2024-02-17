2b5aae92fb14c6f0e6864db889194dd2e2b9d322
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2024-02-17 00:43:42 +0000

    chore: simplify getDB

--- db.js
+++ db.js
@@ -1,13 +1,18 @@
 import { readFileSync, writeFileSync } from 'fs';
 const filename = 'db.json';
 
-export function saveDB (dbName, data) {
+function getDB (dbName) {
   // reads the file
   const db = JSON.parse(readFileSync(filename, 'utf-8') || '{}');  
   // if db is not there, create an empty object
   if (!db) db = {}
   // if a particular database is not there, create an empty object
   if (!db[`${dbName}`]) db[`${dbName}`] = {}
+  return db
+}
+
+export function saveDB (dbName, data) {
+  const db = getDB(dbName);
   // set the data with the id as the key, basically rewriting whatever is in there
   db[`${dbName}`][`${data.id}`] = data;
   // overwrites the file with the new data
@@ -15,19 +20,13 @@ export function saveDB (dbName, data) {
 }
 
 export function readDB (dbName, id = null) {
-  const db = JSON.parse(readFileSync(filename, 'utf-8') || '{}');
-  if (!db) db = {}
-  if (!db[`${dbName}`]) db[`${dbName}`] = {}
-  // if there is id, just return that one
-  if (id) return db[`${dbName}`][`${id}`]
-  // if there is no id, return as an array of values
-  return Object.values(db[`${dbName}`])
+  const db = getDB(dbName);
+  // returns one item or an array of items if id is not null or null respectively
+  return id ? db[`${dbName}`][`${id}`] : Object.values(db[`${dbName}`])
 }
 
 export function deleteItemDB (dbName, id) {
-  const db = JSON.parse(readFileSync(filename, 'utf-8') || '{}');  
-  if (!db) db = {}
-  if (!db[`${dbName}`]) db[`${dbName}`] = {}
+  const db = getDB(dbName)
   // deletes that id from the database
   delete db[`${dbName}`][`${id}`]
   writeFileSync(filename, JSON.stringify(db, null, 2), 'utf-8');
